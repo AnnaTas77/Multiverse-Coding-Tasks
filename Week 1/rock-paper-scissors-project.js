4a7ea5c4b6c2219f759fe396c5b9ee9ebe7633b6
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-05 10:34:13 +0100

    Completed the rock-paper-scissors project. All tests are passing.

---
+++ Week 1/rock-paper-scissors-project.js
@@ -0,0 +1,80 @@
+/*Rock, Paper, Scissors Coding Project
+Directions: Complete the following steps:   
+
+randomChoice()
+GOAL: Create a randomChoice function that returns a random selection of either "rock", "paper", or "scissors" every time the function is called.
+Initialize a function called randomChoice().
+Initialize a variable called choice and store the result of Math.random() in the variable.
+Multiply Math.random by 3.
+Wrap Math.random with Math.floor. Stuck? Click Reveal Content below.
+Based on the value stored in choice:
+Return the string rock if choice has a value of 0.
+Return the string paper if choice has a value of 1.
+Return the string scissors if choice has a value of 2.
+Call the function and console.log() the result to verify that it works!
+Tests 1, 2, and 3 pass at this point
+
+Reveal Content
+winner(player1, player2)
+GOAL: Create a function called winner that takes two player choices of rock, paper, or scissors. The function should return the winner based on the two player choices.
+
+Declare a function called winner that has two parameters: player1 and player2.
+If player1 and player2 have he same value, return the string "It's a tie".
+If the value in player1 beats the value stored in player2, return the string "Player 1 Wins!".
+E.g. If player1 has a value of rock and player2 has a value of scissors, then the function returns "Player 1 Wins!".
+If the value in player2 beats the value stored in player1, return the string "Player 2 Wins!".
+E.g. If player1 has a value of paper and player2 has a value of scissors, then the function returns "Player 2 Wins!".
+Tests 4, 5, and 6 should be passed here.
+
+Play the Game!
+Create a variable called player1 and store the result of randomChoice() in the variable.
+Create a variable called player2 and store the result of a separate call of randomChoice() in the variable.
+Create a variable called result and call winner with the values stored in player1 and player2.
+Log the values of player1, player2, and result to verify your game plays correctly!*/
+
+/** */
+
+const randomChoice = () => {
+  const choice = Math.floor(Math.random() * 3);
+  if (choice === 0) {
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+};
+
+const winner = (player1Choice, player2Choice) => {
+  if (player1Choice === player2Choice) {
+    return "It's a tie";
+  } else if (player1Choice === "rock" && player2Choice === "scissors") {
+    return "Player 1 Wins!";
+  } else if (player1Choice === "paper" && player2Choice === "scissors") {
+    return "Player 2 Wins!";
+  } else if (player1Choice === "paper" && player2Choice === "rock") {
+    return "Player 1 Wins!";
+  } else if (player2Choice === "rock" && player1Choice === "scissors") {
+    return "Player 2 Wins!";
+  } else if (player2Choice === "paper" && player1Choice === "scissors") {
+    return "Player 1 Wins!";
+  } else if (player2Choice === "paper" && player1Choice === "rock") {
+    return "Player 2 Wins!";
+  }
+};
+
+const player1 = randomChoice();
+const player2 = randomChoice();
+
+console.log("Player 1 chose: ", player1);
+console.log("Player 2 chose: ", player2);
+
+const result = winner(player1, player2);
+
+console.log(result);
+
+// DO NOT EDIT CODE BELOW
+module.exports = {
+  randomChoice,
+  winner,
+};
