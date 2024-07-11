6fb84edf3b6b578298dbdf47a045c3e19bd92fce
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-11 22:22:36 +0100

    Updates

--- Week 2/Recursion/recursion-extension.js
+++ Week 2/Recursion/recursion-extension.js
@@ -24,13 +24,29 @@ function countVowels(str) {
   if (vowels.includes(strToArray[0].toLowerCase())) {
     numOfVowels += 1;
   }
- 
+
   const removedFirstChar = strToArray.shift();
   return numOfVowels + countVowels(strToArray);
-
 }
 
 console.log(countVowels("Four score and seven years"));
 
+/** isPalindrome Coding Challenge
+Directions: Complete the following steps:
+
+ 
+
+A palindrome is a word that is spelled the same forwards and backwards. For example, "LEVEL", "RACECAR", and “KAYAK” are all palindromes, while "MOTOR", "RUDDER", and “DOGGED” are not palindromes.
+
+Write a recursive function, isPalindrome, to check if a string is a palindrome.
+Return true if the string is a palindrome; otherwise, return false.
+ */
+
+function isPalindrome(str) {
+  
+}
+
+console.log(isPalindrome("Kayak"));
+
 // DO NOT EDIT BELOW
-module.exports = { countVowels };
+module.exports = { countVowels, isPalindrome };
--- Week 2/Recursion/recursion-extension.test.js
+++ Week 2/Recursion/recursion-extension.test.js
@@ -1,4 +1,4 @@
-const { countVowels } = require("./recursion-extension");
+const { countVowels, isPalindrome } = require("./recursion-extension");
 
 //countVowels
 
@@ -16,4 +16,28 @@ it("returns the number of vowels in the given string", () => {
   expect(returnedValue).toEqual(12);
 });
 
-//
+//isPalindrome
+
+it("is a function", () => {
+  expect(typeof isPalindrome).toEqual("function");
+});
+
+it("returns a boolean", () => {
+  let returnedValue = isPalindrome("definitely not");
+  expect(typeof returnedValue).toEqual("boolean");
+});
+
+it("returns true if the passed string is a palindrome", () => {
+  let returnedValue = isPalindrome("straw warts");
+  expect(returnedValue).toEqual(true);
+});
+
+it("returns false if the passed string is not palindrome", () => {
+  let returnedValue = isPalindrome("this is not a palindrome");
+  expect(returnedValue).toEqual(false);
+});
+
+it("ignores differences in case", () => {
+  let returnedValue = isPalindrome("UFOtofu");
+  expect(returnedValue).toEqual(true);
+});
