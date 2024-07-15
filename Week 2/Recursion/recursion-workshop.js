7f9bbfaf6abafd8682fdc385d8755caa72c22207
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-15 16:25:28 +0100

    Solved the exponent recursion task

--- Week 2/Recursion/recursion-workshop.js
+++ Week 2/Recursion/recursion-workshop.js
@@ -20,5 +20,28 @@ function sumNums(num) {
 console.log(sumNums(10)); //55
 console.log(sumNums(15)); //120
 
+/** Recursive Exponent Coding Challenge 
+Directions: Complete the activity below:
+
+Write a recursive function named exponent which will take two numbers as parameters. The first number is the base and the second number is the exponent.
+The base case is that any base number raised to exponent 0, results in 1.
+Your function is to return the value of the base number raised to the exponent number.
+Run your code.
+Test your code.
+Examples
+exponent(4,2) -> 16
+exponent(3,3) -> 27
+exponent(2,8) -> 256 */
+
+function exponent(baseNum, exponentNum) {
+  if (exponentNum === 0) {
+    return 1;
+  }
+
+  return baseNum * exponent(baseNum, exponentNum - 1);
+}
+
+console.log(exponent(4, 2));
+
 // DO NOT EDIT CODE BELOW
-module.exports = { sumNums };
+module.exports = { sumNums, exponent };
--- Week 2/Recursion/recursion-workshop.test.js
+++ Week 2/Recursion/recursion-workshop.test.js
@@ -1,4 +1,6 @@
-const {sumNums} = require("./recursion-workshop");
+const { sumNums, exponent } = require("./recursion-workshop");
+
+//sumNums
 
 it("Returns a Function", () => {
   expect(typeof sumNums).toEqual("function");
@@ -22,3 +24,35 @@ it("Passes Recursive Case", () => {
   expect(sumNums(50)).toEqual(1275);
   expect(sumNums(37)).toEqual(703);
 });
+
+// exponent
+it("Returns a Function", () => {
+  expect(typeof exponent).toEqual("function");
+});
+
+test("Returns a Number", () => {
+  expect(typeof exponent(2, 4)).toEqual("number");
+});
+
+test("test stub", () => {
+  expect(exponent(5, 0)).toEqual(1);
+  expect(exponent(13, 0)).toEqual(1);
+  expect(exponent(100, 0)).toEqual(1);
+  expect(exponent(43, 0)).toEqual(1);
+});
+
+it("Passes Example Cases", () => {
+  expect(exponent(4, 2)).toEqual(16);
+  expect(exponent(3, 3)).toEqual(27);
+  expect(exponent(2, 8)).toEqual(256);
+});
+
+it("Passes Other Test Cases", () => {
+  expect(exponent(5, 2)).toEqual(25);
+  expect(exponent(12, 2)).toEqual(144);
+  expect(exponent(2, 4)).toEqual(16);
+  expect(exponent(5, 3)).toEqual(125);
+  expect(exponent(100, 3)).toEqual(1000000);
+});
+
+//
