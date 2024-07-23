dccfe1011f931368590148bbc12fbe0d241ea911
Anna Tasheva <anna.tasheva90@gmail.com>
2024-07-23 14:35:12 +0100

    Added an extra example

--- Week 4/Inheritance and Static Methods/extra.js
+++ Week 4/Inheritance and Static Methods/extra.js
@@ -19,3 +19,23 @@ const bob = new Student("Bob", 20, "Computer Science");
 
 console.log(alice.species);
 console.log(bob.species);
+
+class Calculator {
+  static add(a, b) {
+    return a + b;
+  }
+
+  static subtract(a, b) {
+    return a - b;
+  }
+}
+
+class ScientificCalculator extends Calculator {
+  static multiply(a, b) {
+    return a * b;
+  }
+}
+
+console.log(Calculator.add(2, 3));
+console.log(Calculator.subtract(5, 2));
+console.log(ScientificCalculator.multiply(2, 3));
