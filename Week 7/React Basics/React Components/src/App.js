46a0d6d5188915e921a0ff1af759427357e25aa9
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-12 15:55:16 +0100

    Added the Task 1 from todays module. All tests are passing.

---
+++ Week 7/React Basics/React Components/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// Import components here
+import Title from "./components/Title.js";
+import Content from "./components/Content.js";
+
+function App() {
+  return (
+    <>
+      <Title />
+      <Content />
+      <Content />
+    </>
+  );
+}
+
+export default App;
