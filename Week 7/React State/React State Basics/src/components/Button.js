0585fb87db17ce8927940c84cd3895dfcd192edc
Anna Tasheva <anna.tasheva90@gmail.com>
2024-08-13 09:22:02 +0100

    Solved Task 1 from React State Module. All tests are passing.

---
+++ Week 7/React State/React State Basics/src/components/Button.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+// Import useState here
+
+function Button() {
+  // Initialize state here
+  const [count, setCount] = useState(0);
+
+  // Create handleClick function here
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      {/* Render count state in the h1*/}
+      <h1>Clicked {count} times</h1>
+      {/* Call handleClick function when clicking the button*/}
+      <button onClick={handleClick}>Click Me</button>
+    </>
+  );
+}
+
+export default Button;
