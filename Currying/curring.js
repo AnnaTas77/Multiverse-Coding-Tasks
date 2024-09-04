c7cf0a7bab17d40af9585f51f40d08747f905d70
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-04 14:09:16 +0100

    Added additional comments about currying.

--- Currying/curring.js
+++ Currying/curring.js
@@ -9,9 +9,9 @@ function curriedFn(param1) {
 const result1 = curriedFn(1)(2)(3);
 console.log("Result 1: ", result1);
 
-const addFive = curriedFn(5);
-const addFiveAndTen = addFive(10);
-const result2 = addFiveAndTen(3);
+const addFive = curriedFn(5); // returns a function that expects 'param2'
+const addFiveAndTen = addFive(10); // returns a function that expects 'param3'
+const result2 = addFiveAndTen(3); // computes the sum of 5, 10 and 3.
 
 console.log("Result 2: ", result2);
 
